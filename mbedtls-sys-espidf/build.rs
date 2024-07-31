1fcd3f91395a9a208b1c0e922e175c4afb4368a4
Volkalex28 <anan20000228@gmail.com>
2024-08-01 00:03:55 +0300

    Idf v5.2.2

--- mbedtls-sys-espidf/build.rs
+++ mbedtls-sys-espidf/build.rs
@@ -1,3 +1,9 @@
 fn main() -> anyhow::Result<()> {
+    println!("cargo::rustc-check-cfg=cfg(esp_idf_mbedtls_hardware_aes)");
+    println!("cargo::rustc-check-cfg=cfg(esp_idf_mbedtls_hardware_sha)");
+    println!("cargo::rustc-check-cfg=cfg(esp_idf_mbedtls_ecdh_legacy_context)");
+    println!("cargo::rustc-check-cfg=cfg(esp_idf_version_full, values(any()))");
+    println!("cargo::rustc-check-cfg=cfg(esp_idf_version_minor, values(any()))");
+
     embuild::build::CfgArgs::output_propagated("ESP_IDF")
 }
